ba81c4fc3dc3fd4efa9e5001bae5fd2d98608249
ITDaos <jjok730@gmail.com>
2018-05-03 10:17:18 +0300

    Deploy (#3)

--- scripts/connect_DB.php
+++ scripts/connect_DB.php
@@ -1,12 +1,17 @@
-<?
+<?php
+	## function declare
+	function createUser($link, $token, $given_name, $last_name) {
+		$query = "INSERT INTO `users` (`id`, `token`, `role`, `given_name`, `last_name`) VALUES (NULL, '$token', 'user', '$given_name', '$last_name');";
+		mysqli_query($link, $query);
+	}
 
-	$host = "https://frozen-reef-64159.herokuapp.com";
-	$login = "root";
-	$password = "";
-	$DB = "olimp-project-backend";
-	$link = mysqli_connect($host, $login, $password, $DB);
+	$host = "mj235895.mysql.ukraine.com.ua";
+	$login = "mj235895_stud18";
+	$password = "uad2lfjh";
+	$DB = "mj235895_stud18";
+	$DB_CONNECTION = mysqli_connect($host, $login, $password, $DB);
 
-	if (!$link) {
+	if (!$DB_CONNECTION) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
