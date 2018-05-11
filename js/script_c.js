a4538051517781af9f9cc53152961f9f73c1fd97
MykhailKo <38505816+MykhailKo@users.noreply.github.com>
2018-05-11 16:51:42 +0300

    New script for Calculator

--- js/script_c.js
+++ js/script_c.js
@@ -1,25 +1,47 @@
 jQuery('document').ready(function(){
 	jQuery('button').on('click', function(){
 		var bal1, bal2, bal3, bal4, bal5, res;
+		res = 0;
+		localStorage.setItem('bal', res);
 		bal1 = jQuery('#bal1').val();
 		bal2 = jQuery('#bal2').val();
 		bal3 = jQuery('#bal3').val();
 		bal4 = jQuery('#bal4').val();
 		bal5 = jQuery('#bal5').val();
-		if(jQuery('#bal5').val() == "" || jQuery('#bal4').val() == "" || jQuery('#bal3').val() == ""
-			|| jQuery('#bal2').val() == "" || jQuery('#bal1').val() == "" || jQuery('#bal5').val() > 200 || jQuery('#bal4').val() > 200 || jQuery('#bal3').val() > 200
-			|| jQuery('#bal2').val() > 200 || jQuery('#bal').val() > 200 || jQuery('#bal5').val() < 100 || jQuery('#bal4').val() < 100 || jQuery('#bal3').val() < 100
-			|| jQuery('#bal2').val() < 100 || jQuery('#bal').val() < 100){
-		if(jQuery('#bal5').val() == "" || jQuery('#bal4').val() == "" || jQuery('#bal3').val() == ""
-			|| jQuery('#bal2').val() == "" || jQuery('#bal1').val() == ""){
+		if(jQuery('#bal5').val() == "" 
+			|| jQuery('#bal4').val() == "" 
+			|| jQuery('#bal3').val() == ""
+			|| jQuery('#bal2').val() == "" 
+			|| jQuery('#bal1').val() == "" 
+			|| jQuery('#bal5').val() > 200 
+			|| jQuery('#bal4').val() > 200 
+			|| jQuery('#bal3').val() > 200
+			|| jQuery('#bal2').val() > 200 
+			|| jQuery('#bal').val() > 200 
+			|| jQuery('#bal5').val() < 100 && jQuery('#bal5').val() > 1 
+			|| jQuery('#bal4').val() < 100 && jQuery('#bal4').val() > 1
+			|| jQuery('#bal3').val() < 100 && jQuery('#bal3').val() > 1
+			|| jQuery('#bal2').val() < 100 && jQuery('#bal2').val() > 1
+			|| jQuery('#bal1').val() < 100 && jQuery('#bal1').val() > 1){
+		if(jQuery('#bal5').val() == "" 
+			|| jQuery('#bal4').val() == "" 
+			|| jQuery('#bal3').val() == ""
+			|| jQuery('#bal2').val() == "" 
+			|| jQuery('#bal1').val() == ""){
 			alert("Введіть дані, якщо у вас немає цих балів, введіть 0.");
 		}
-		if(jQuery('#bal5').val() > 200 || jQuery('#bal4').val() > 200 || jQuery('#bal3').val() > 200
-			|| jQuery('#bal2').val() > 200 || jQuery('#bal').val() > 200){
+		if(jQuery('#bal5').val() > 200 
+			|| jQuery('#bal4').val() > 200 
+			|| jQuery('#bal3').val() > 200
+			|| jQuery('#bal2').val() > 200 
+			|| jQuery('#bal').val() > 200){
 			alert("Бали ЗНО, ФДП та атестату не можуть перевищувати 200.");
 		}
-		if(jQuery('#bal5').val() < 100 || jQuery('#bal4').val() < 100 || jQuery('#bal3').val() < 100
-			|| jQuery('#bal2').val() < 100 || jQuery('#bal').val() < 100){
+		if(jQuery('#bal5').val() < 100 && jQuery('#bal5').val() > 1 
+			|| jQuery('#bal4').val() < 100 && jQuery('#bal4').val() > 1
+			|| jQuery('#bal3').val() < 100 && jQuery('#bal3').val() > 1
+			|| jQuery('#bal2').val() < 100 && jQuery('#bal2').val() > 1
+			|| jQuery('#bal1').val() < 100 && jQuery('#bal1').val() > 1){
 			alert("Бали ЗНО, ФДП та атестату не можуть бути меньше 100.");
 		}
 			jQuery('#res').val("Невірні дані");
@@ -35,6 +57,7 @@ jQuery('document').ready(function(){
 		bal4 = bal4 * 0.05;
 		bal5 = bal5 * 0.05;
 		res = bal1 + bal2 + bal3 + bal4 + bal5;
+		localStorage.setItem('bal', res);
 		jQuery('#res').val(res);
 	}
 	});
