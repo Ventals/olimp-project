147549cf06aa27edc23b77d83409520fc9604659
Mykyta-Novikov <38505408+Mykyta-Novikov@users.noreply.github.com>
2018-05-12 12:24:45 +0300

    Changes in document collection quest.

--- js/DocumentCollectionScript.js
+++ js/DocumentCollectionScript.js
@@ -1,4 +1,43 @@
-function clickInRequired(elem) {
+var documents = [
+	{
+		name: "1",
+		description: "1st document",
+		src: "images/bag.png",
+		requered: true
+	},
+	{
+		name: "2",
+		description: "2nd document",
+		src: "",
+		requered: true
+	},
+	{
+		name: "3",
+		description: "3rd document",
+		src: "images/nout.png",
+		requered: true
+	}
+];
+
+
+
+for (var i = 0; i < documents.length; i++) {
+	let img = document.createElement("img");
+	img.src = documents[i].src;
+	img.doc = documents[i];
+	if (documents[i].requered) {
+		img.onmouseover = mouseOverInRequired;
+		img.onmouseout = mouseOutInRequired; 
+		img.onclick = clickInRequired;
+	} 
+	else {
+		img.onmouseout = mouseOverInRequired;
+		img.onmouseover = mouseOutInRequired; 
+	}
+	document.getElementById("questImages").appendChild(img);
+}
+function clickInRequired(event) {
+	var elem = event.currentTarget;
 	var t = performance.now();
 	var startWidth = elem.width;
 	requestAnimationFrame(function anim(time) {
@@ -8,15 +47,16 @@ function clickInRequired(elem) {
 			setInterval(function() {
 				elem.hidden = true;
 				setElementIsComleted(elem);
-			}, CLICK_PAUSE_BRFORE_HIDDEN);
+			}, CLICK_PAUSE_BEFORE_HIDDEN);
 			return;
 		}
 		elem.width = startWidth + clickTimeFunction(time/CLICK_ANIMATION_DURATION)*CLICK_SIZE_CHANGE*startWidth;
 		requestAnimationFrame(anim);
 	});
 }
-	
-function mouseOverInRequired(elem) {
+
+function mouseOverInRequired(event) {
+	var elem = event.currentTarget;
 	var t = performance.now();
 	var startWidth = elem.width;
 	requestAnimationFrame(function anim(time) {
@@ -29,24 +69,28 @@ function mouseOverInRequired(elem) {
 		requestAnimationFrame(anim);
 	});
 }
-function mouseOutInRequired(elem) {
+function mouseOutInRequired(event) {
+	var elem = event.currentTarget;
 	var t = performance.now();
 	var startWidth = elem.width;
 	requestAnimationFrame(function anim(time) {
 		time = time - t;
 		if(time >= MOUSE_MOVE_ANIMATION_DURATION) {
-			elem.width = (1 - MOUSE_MOVE_SIZE_CHANGE)*startWidth;
+			elem.width = (1/(1 + MOUSE_MOVE_SIZE_CHANGE))*startWidth;
 			return;
 		}
-		elem.width = startWidth - 0.1*mouseMoveTimeFunction(time/MOUSE_MOVE_ANIMATION_DURATION)*MOUSE_MOVE_SIZE_CHANGE*startWidth;
+		elem.width = startWidth - mouseMoveTimeFunction(time/MOUSE_MOVE_ANIMATION_DURATION)*MOUSE_MOVE_SIZE_CHANGE*startWidth;
 		requestAnimationFrame(anim);
 	});
 }
-
-
 function setElementIsComleted(elem) {
-	
+	document.getElementById("nameOut").value = elem.doc.name;
+	document.getElementById("descriptionOut").value = elem.doc.description;
+	document.getElementById("dialog").hidden = false;
 }
+
+
+
 function clickTimeFunction(time) {
 	return time;
 }
@@ -57,4 +101,4 @@ var CLICK_SIZE_CHANGE = 0.25;
 var MOUSE_MOVE_SIZE_CHANGE = 0.1;
 var CLICK_ANIMATION_DURATION = 1000;
 var MOUSE_MOVE_ANIMATION_DURATION = 500;
-var CLICK_PAUSE_BRFORE_HIDDEN = 150;
+var CLICK_PAUSE_BEFORE_HIDDEN = 150;
