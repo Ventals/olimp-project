7e63f6b7c18eba486900dd36259191377799d995
Mike <jjok730@gmail.com>
2018-05-12 20:16:29 +0300

    OP-00: favicon added

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
   use Auth0\SDK\Auth0;
 
+
   $domain        = getenv('AUTH0_DOMAIN');
   $client_id     = getenv('AUTH0_CLIENT_ID');
   $client_secret = getenv('AUTH0_CLIENT_SECRET');
@@ -41,10 +42,14 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="style_c.css">
     <link rel="alternate stylesheet" type="text/css" href="stylecab.css" id="cab_style">
+    <link rel="icon" 
+          type="image/ico" 
+          href="images/favicon.ico">
+
     <script src="js/jquery-3.3.1.slim.js" type="text/javascript"></script>
-    <script type="text/javascript" src="js/script1.js"></script>
     <script type="text/javascript" src = "js/cabinetScript.js"> </script>
     <script type="text/javascript" src="js/script.js"></script>
+
 </head>
 <body>
     
@@ -57,12 +62,12 @@
     <div class="block1" id="block1">
       <div class = "buttons">
         <p class = "logo-main">  </p>
-        <li><a href="#registrationFormBlock">
+        <li><a href="login.php">
           <ul class="in">
             
           </ul>
          </a></li>
-        <li><a  href="#registrationFormBlock">
+        <li><a  href="login.php">
           <ul class="reg">
 
           </ul>
@@ -119,15 +124,19 @@
           </a>
         </div>
         <form class = "registrationForm">
-          <input type = "text" class="input" placeholder="Ваше пошта" id="email">
-          <input type = "password" class="input" placeholder="Пароль" id="pass">
-          <div class = "orline"></div>
-          <div class="soc">
-            <a href="https://www.facebook.com/"><li class="fb"></li></a>
-            <a href="https://twitter.com/login?lang=ru"><li class="tw"></li></a>
-            <a href="https://myaccount.google.com/?hl=ru"><li class="goo"></li></a>
-          </div>
-        </form>
+	
+					<div class = "orline">
+						
+					</div>
+					<div class="socshare">
+						Поділіться з друзями в соц-мережах
+					</div>
+					<div class="soc">
+						<a href="https://www.facebook.com/"><li class="fb"></li></a>
+						<a href="https://twitter.com/login?lang=ru"><li class="tw"></li></a>
+						<a href="https://myaccount.google.com/?hl=ru"><li class="goo"></li></a>
+					</div>
+				</form>
       </div>
     </div>
    
@@ -139,72 +148,174 @@
     <script>
       document.getElementById("cab_style").setAttribute('rel', 'stylesheet');
     </script>
-    <ul class="out">
-              
-    </ul>
-    <ul class="form">
-    <li>
-      <img src="images/comp.png"> 
-      <h2>Комп'ютер</h2>
-      На комп'ютері ви зможете побачити карту квесту...
-    </li>
-    <li>
-      <img src="images/nout.png">
-      <h2>Ноутбук</h2>
-      На ноутбуці ви знайдете...
-    </li>
-    <li>
-      <img src="images/board.jpg">
-      <h2>Дошка</h2>
-      Дошка показує вам...
-    </li>
-    <li>
-      <img src="images/bag.png">
-      <h2>Портфель</h2>
-      Портфель потрібен для того щоб...
-    </li>
-    <li>
-      <img src="images/zno.png">
-      <h2>Збірник ЗНО</h2>
-      В збірнику тестів ЗНО ви зможете підготуватись до здачі...
-    </li>
-    </ul>
-    <div class="help">
-    Підказка
-    </div>
-    <div class = "room">
-    <div>
-      <p class = "title"> Кімната </p>
-      <a href="logout.php">
-    <div class="logo">
-    </div>
-    </a>
-    </div>
-    <div class="board" id = "table"
-    onmouseover = "tableMouseOver()" onmouseout = "tableMouseOut()" onclick = "tableClick()">
+    
+	<ul class="out">
+									
+			</ul>
+			<ul class="form">
+				<li>
+					<img src="images/comp.png">	
+					<h2>Комп'ютер</h2>
+					В комп'ютері починається перший етап квесту, вибір професії.
+				</li>
+				<li>
+					<img src="images/nout.png">
+					<h2>Ноутбук</h2>
+					Ноутбук відповідає за вибір ВНЗ на карті України, залежить від проіесії.
+				</li>
+				<li>
+					<img src="images/board.jpg">
+					<h2>Дошка</h2>
+					Дошка показує вам інформацію про ваші останні дії та вибори.
+				</li>
+				<li>
+					<img src="images/bag.png">
+					<h2>Портфель</h2>
+					Портфель потрібен для того щоб побачити список доступних і пройдених квестів.
+				</li>
+				<li>
+					<img src="images/zno.png">
+					<h2>Збірник ЗНО</h2>
+					В збірнику тестів ЗНО ви зможете підготуватись до здачі, здавши екзамени потрібні для вашого вступу.
+				</li>
+			</ul>
+			<ul class="compscreen">
+				<ul class="next" id="next1">Наступна професія</ul>
+				<ul class="next" id="next2">Наступна професія</ul>
+				<ul class="next" id="next3">Наступна професія</ul>
+				<ul class="slider">
+					<li class="prof1">
+						<img src="images/prof1.jpg">
+						<p>
+							Юрист - це фахівець в галузі права. Він знає закони і правові норми, вміє їх використовувати і здатний навчати їх основам інших. Це експерт в області юриспруденції, який може займати будь-яку правову посаду, від адвоката до судді. Для цього необхідно отримати вищу юридичну освіту.
+						</p>
+						<ul class="menu">
+						<ul class="choose" id="choose1">
+							Обрати професію
+						</ul>
+					</ul>
+					</li>
+					<li class="prof2">
+						<img src="images/prof2.jpg">
+						<p>
+							Програміст - це фахівець, який займається розробкою програмного забезпечення для персональних, вбудованих, промислових і інших різновидів комп'ютерів, тобто програмуванням. Прикладний - це фахівець, який здійснює розробку і налагодження програм для вирішення різних завдань.
+						</p>
+						<ul class="menu">
+						<ul class="choose" id="choose2">
+							Обрати професію
+						</ul>
+					</ul>
+					</li>
+					<li class="prof3">
+						<img src="images/prof3.jpg" class="bigimg">
+						<p>
+							Механік - кваліфікований робітник, який має професійну освіту і зайнятий обслуговуванням, ремонтом технічних засобів (необов'язково пов'язаних з механізмами), наприклад: автомеханік, авіамеханік, електромеханік, радіомеханік, механік по ремонту побутової техніки і т. Д.
+						</p>
+						<ul class="menu">
+						<ul class="choose" id="choose3">
+							Обрати професію
+						</ul>
+					</ul>
+					</li>
+				</ul>
+			
 
-    </div>
+			</ul>
+			<div class="noutscreen">
+				<p id = "title"> Оберіть ВНЗ </p>
+				<div id = "map"> </div>
+				<div hidden id = "dialog">
+					<p> <output id = "name"> </output> </p> 
+					<img id = "image">
+					<p> <output id = "description"> </output> </p>
+					<button onclick = "VNZSelected()" class = "vnzbtn" id = "choiseadd"> Обрати цей ВНЗ </button>
+					<button onclick = "backToMap()" class = "vnzbtn"> Повенутися до мапи </button>
+				</div>
+				<script
+					src = "js/MapInCabinetScript.js"> </script>
+				<script async defer
+					src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhB6Pq1U-G2R70VstNVDnESvSCePbjQd0&callback=initMap"> </script>
+			</div>
 
-    <div class="comp" id = "computer"
-    onmouseover = "computerMouseOver()" onmouseout = "computerMouseOut()"onclick = "computerClick()">
-    </div>
+			<div class="znoscreen">
+				<h3>Іспити ЗНО</h3>
+				<p>Тут ви можете здати ЗНО з потрібних вам предметів, які зазначені у вікні вибору ВНЗ, але й пройти усі ці екзамени буде корисно для кожного. Онлайн тестування не містить деяких завдань, максимальна оцінка за укр.мову 180 балів(відсутній твір), з математики - 190 балів(відсутнє відкрите завдвння).</p>
+				<div class="bookshelf">
+				<li id = "book1"><a href="https://zno.osvita.ua/ukrainian/281/">Українська мова та література</a></li>
+				<li id = "book2"><a href="https://zno.osvita.ua/mathematics/290/">Математика</a></li>
+				<li id = "book3"><a href="https://zno.osvita.ua/ukraine-history/282/">Історія України</a></li>
+				<li id = "book4"><a href="https://zno.osvita.ua/english/283/">Англійська мова</a></li>
+				<li id = "book5"><a href="https://zno.osvita.ua/physics/291/">Фізика</a></li>
+			</div>
+			</div>
 
-    <div class="laptop" id = "laptop"
-    onmouseover = "laptopMouseOver()" onmouseout = "laptopMouseOut()" onclick = "laptopClick()">
-    </div>
+			<div class="out1">
 
-    <div class="zno" id = "ZNO"
-    onmouseover = "ZNOMouseOver()" onmouseout = "ZNOMouseOut()" onclick = "ZNOClick()">
-    </div>
+			</div>
+			<div class="mesform">
 
-    <div class="bag" id = "briefcase"
-    onmouseover = "briefcaseMouseOver()" onmouseout = "briefcaseMouseOut()" onclick = "briefcaseClick()">
-    </div>
+			</div>
 
-    </div>
-    <a class="link" href = "calculator.html">
-    Розрахунок конкурсного балу
-    </a>
+			<div class="mom">
+				<ul class="talk">
+					
+				</ul>
+			</div>
+
+			<div class="dad">
+				<ul class="talk1">
+
+				</ul>
+			</div>
+
+			<div class="help">
+			Підказка
+		</div>
+		<div class = "room">
+			<div>
+			<p class = "title"> Кімната </p>
+			<a href="" class="support">Підтримка vnzquest@gmail.com</a>
+			<a href="StartPageRaedy.html">
+			<div class="logo">
+		</div>
+	</a>
+		</div>
+			<div class="board" id = "table">
+				<div class="prof">
+					
+				</div>
+				<div class="vnz">
+					
+				</div>
+				<div class="cbal">
+					
+				</div>
+		</div>
+
+			<div class="comp" id = "computer">
+		</div>
+
+			<div class="laptop" id = "laptop">
+		</div>
+
+			<div class="zno" id = "ZNO">
+		</div>
+
+			<div class="bag" id = "briefcase">
+		</div>
+
+		</div>
+		<div class = "back">
+			Назад
+		</div>
+		<a class="link" href = "calculator.html">
+			Розрахунок конкурсного балу
+			</a>
+		<a href="StartPageRaedy.html">
+		<div class="exit">
+
+		</div>
+	</a>
 
 
     <!-- ************************************************************************* -->
